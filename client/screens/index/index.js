7294ec091dff932ed2eef5914a769daf09db3420
Calvin Ku <cjwku@stu.ust.hk>
2018-04-21 15:13:10 +0800

    basic component complete

--- client/screens/index/index.js
+++ client/screens/index/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, ButtonGroup, Form, ListGroup, ListGroupItem} from 'react-bootstrap';
 
 export class Index extends React.Component {
 
@@ -8,7 +9,46 @@ export class Index extends React.Component {
 
     render() {
         return (
-            <div/>
+        	<div>
+		        <div class="header">
+				        <ButtonGroup bsSize="big" class="buttonGroup">
+					        <Button class="custom-button">File</Button>
+					        <Button>Insert</Button>
+					        <Button>Reference</Button>
+				        </ButtonGroup>
+		        </div>
+			        <Form>
+				        <textarea class="left-container">
+                            Hello there, you can begin typing here...
+						</textarea>
+			        </Form>
+
+		        <div class="right-container">
+			        <div class="title">
+				        Fact Confidence Level:
+			        </div>
+			        <div class="title">
+				        Suggest Article:
+			        </div>
+			        <ListGroup>
+				        <ListGroupItem bsStyle="warning" header="Heading 1">
+					        Item 1
+					        <text>
+						        sup
+					        </text>
+					        <text>
+						        hi
+					        </text>
+				        </ListGroupItem>
+				        <ListGroupItem bsStyle="warning" header="Heading 1">
+					        Item 2
+				        </ListGroupItem>
+				        <ListGroupItem bsStyle="warning" header="Heading 1">
+					        ...
+				        </ListGroupItem>
+			        </ListGroup>
+		        </div>
+	        </div>
         )
     }
 
