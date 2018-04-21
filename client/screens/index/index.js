6399084cd963427ffd73d0a1de5c99c4203e0a88
Calvin Ku <cjwku@stu.ust.hk>
2018-04-21 17:26:10 +0800

    refine frontend

--- client/screens/index/index.js
+++ client/screens/index/index.js
@@ -5,40 +5,65 @@ export class Index extends React.Component {
 
     constructor(props) {
         super(props);
+	    this.state = {
+	    	text: '',
+		    title: ''
+	    };
     }
 
     render() {
         return (
-        	<div>
-		        <div class="header">
-				        <ButtonGroup bsSize="big" class="buttonGroup">
-					        <Button class="custom-button">File</Button>
-					        <Button>Insert</Button>
-					        <Button>Reference</Button>
-				        </ButtonGroup>
+        	<div className="index">
+		        <div class="left-container">
+			        <div class="header">
+				        <div class="header-icon">
+				            <img src="/img/logo.png" alt="logo.png" height="70"/>
+				        </div>
+				        <div className="header-buttons">
+					        <input type="title" placeholder="Report Title"/>
+					        <div className="buttonGroup">
+						        <button className="custom-button">
+							        File
+						        </button>
+						        <button className="custom-button">
+							        Edit
+						        </button>
+						        <button className="custom-button">
+							        Insert
+						        </button>
+						        <button className="custom-button">
+							        Format
+						        </button>
+						        <button className="custom-button">
+							        Tools
+						        </button>
+						        <button className="custom-button">
+							        Help
+						        </button>
+					        </div>
+				        </div>
+			        </div>
+			        <textarea
+				        className="text-area"
+				        onChange={(event) => {
+					        this.setState({
+						        text: event.target.value
+					        });
+				        }}
+			        >
+	                Hello there, you can begin typing here...
+		        </textarea>
 		        </div>
-			        <Form>
-				        <textarea class="left-container">
-                            Hello there, you can begin typing here...
-						</textarea>
-			        </Form>
-
-		        <div class="right-container">
-			        <div class="title">
+		        <div className="right-container">
+			        <div className="title">
 				        Fact Confidence Level:
 			        </div>
-			        <div class="title">
+			        <div className="title">
 				        Suggest Article:
 			        </div>
 			        <ListGroup>
 				        <ListGroupItem bsStyle="warning" header="Heading 1">
 					        Item 1
-					        <text>
-						        sup
-					        </text>
-					        <text>
-						        hi
-					        </text>
 				        </ListGroupItem>
 				        <ListGroupItem bsStyle="warning" header="Heading 1">
 					        Item 2
